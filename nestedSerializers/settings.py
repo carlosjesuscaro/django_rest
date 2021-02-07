7c9d1918d7fdb9027fa724082b2d8d54caf06e3a
carloscaro <carlosjesuscaro@icloud.com>
2021-02-07 15:37:13 -0500

    settings

--- nestedSerializers/settings.py
+++ nestedSerializers/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nsApp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,10 @@ WSGI_APPLICATION = 'nestedSerializers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studentdb',
+        'USER': 'root',
+        'PASSWORD': 'M@rc85A8r3!185'
     }
 }
 
