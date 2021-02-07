bf94d34338b556902a3aaac1379897bc726ada8b
carloscaro <carlosjesuscaro@icloud.com>
2021-02-07 16:23:07 -0500

    views fix

--- nsApp/urls.py
+++ nsApp/urls.py
@@ -19,8 +19,8 @@ from nsApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('author/', views.AuthorListView.as_view())
-    path('author/<int:pk>', views.AuthorDetailView.as_view())
-    path('book/', views.BookListView.as_view())
+    path('author/', views.AuthorListView.as_view()),
+    path('author/<int:pk>', views.AuthorDetailView.as_view()),
+    path('book/', views.BookListView.as_view()),
     path('book/<int:pk>', views.BookDetailView.as_view())
 ]
