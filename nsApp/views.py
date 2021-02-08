b1d301b7c733e30bd729b2ebf6c2174547d118f5
carloscaro <carlosjesuscaro@icloud.com>
2021-02-07 20:17:54 -0500

    adding security

--- nsApp/views.py
+++ nsApp/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from .models import Author, Book
 from nsApp.serializer import AuthorSerializer, BookSerializer
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class AuthorDetailView(generics.RetrieveDestroyAPIView):
     queryset = Author.objects.all()
